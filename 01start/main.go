d26cdba9fb65f80392b8d5bb84252438688edf28
Tatiana Nikolaeva <tatiana.nikolaeva@gmx.net>
2025-03-15 23:34:43 +0100

    Guessing game. Random with seed.Current time.

--- 01start/main.go
+++ 01start/main.go
@@ -4,12 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"helloworld/doctor"
+	"math/rand"
 	"os"
 	"strings"
 )
 
-func main() {
+const prompt = " and press ENTER"
 
+func main() {
+	guessingGame()
+}
+
+func guessingGame() {
+	random := rand.New(rand.NewSource(123))
+
+	firstNum := random.Intn(8) + 2
+	secondNum := random.Intn(8) + 2
+	subtraction := random.Intn(8) + 2
+	answer := firstNum*secondNum - subtraction
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Let's play the guessing game. Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Multiply your number by ", firstNum, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNum, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("And the answer is ", answer)
+
+}
+
+func chatbotElisa() {
 	fmt.Println("Welcome to the chatbot")
 	reader := bufio.NewReader(os.Stdin)
 
@@ -30,7 +65,6 @@ func main() {
 		}
 
 	}
-
 }
 
 func demoVariableAssignment() {
